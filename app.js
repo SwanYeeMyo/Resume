258094dfc362e0ba5aa62d898c3cfcffc2a8946b
Dimitri <swanyeemyo2001@gmail.com>
2022-06-13 19:43:19 +0630

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var typed = new Typed(".auto-type", {
+    strings: [" A Junior Developer", " A Full Stack Developer", " A Student and Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 5,
+    loop: true,
+})
+var skill = new Typed(".skill-type", {
+    strings: ["Coding", "Reading", "and Debugging"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true
+});
+
+  AOS.init({
+    
+  });
+
+//   tooltip
